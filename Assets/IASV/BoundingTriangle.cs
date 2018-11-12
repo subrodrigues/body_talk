6f203c43324a96ef540a58d045e558aa8f484c48
Filipe Rodrigues <subrodrigues@gmail.com>
2018-11-12 21:11:14 +0000

    [new] Initial code Info: Need to import the following unitypackages: LeapMotion, Oculus, SteamVR and SteamVR_Input (this one is auto included)

---
+++ Assets/IASV/BoundingTriangle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingTriangle : MonoBehaviour {
+	public GameObject leftHandObj, rightHandObj, headObj;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		printWorldPos (leftHandObj);
+		printWorldPos (rightHandObj);
+		printWorldPos (headObj);
+	}
+
+	void printWorldPos(GameObject obj){
+		Debug.Log (obj.transform.position.ToString("F4"));
+	}
+}
