933efcd7b58466209567e8db39cdabcac43957b0
Filipe Rodrigues <subrodrigues@gmail.com>
2018-12-17 00:25:23 +0000

    [new] Billboard for Emotional rendering

---
+++ Assets/IASV/Scripts/EmotionUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmotionUpdater : MonoBehaviour {
+   	public Texture[] textures;
+    public float changeInterval = 0.33F;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+	public void SetEmotion(int index){
+		if (textures.Length == 0)
+            return;
+        rend.material.mainTexture = textures[index];
+	}
+}
